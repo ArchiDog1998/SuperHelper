e8c057c0e60189f170a7de090ea0cc81b2f6d888
秋水 <1123993881@qq.com>
2022-08-04 22:19:45 +0800

    1.1.1 release.

--- SuperHelper/SuperHelperInfo.cs
+++ SuperHelper/SuperHelperInfo.cs
@@ -32,7 +32,7 @@ namespace SuperHelper
         //Return a string representing your preferred contact details.
         public override string AuthorContact => "1123993881@qq.com";
 
-        public override string Version => "1.1.0";
+        public override string Version => "1.1.1";
     }
 
     public class SuperHelperAssemblyPriority : GH_AssemblyPriority
